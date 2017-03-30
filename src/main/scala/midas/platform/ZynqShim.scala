ba3177f95f118a19aafa09b7a1d6112121d78696
Sagar Karandikar <sagark@eecs.berkeley.edu>
2017-03-30 10:36:15 -0700

    implement writes in nastiumi

--- src/main/scala/midas/platform/ZynqShim.scala
+++ src/main/scala/midas/platform/ZynqShim.scala
@@ -68,16 +68,78 @@ class SimUMIMem(implicit p: Parameters) extends Module {
 /* todo move to firesim */
 class NastiUMIAdapter(implicit p: Parameters) extends Module {
   val io = IO(new Bundle {
-    val nastimem = Flipped(new NastiIO()(p alter Map(NastiKey -> p(SlaveNastiKey))))
+    val nastimem = Flipped(new NastiIO)
 		
 		val umireq = Decoupled(new CatapultMemReq)
-		val umiresp = Flipped(Decoupled(new CatapultMemResp))    
+		val umiresp = Flipped(Decoupled(new CatapultMemResp))
   })
 
-  io.nastimem.ar.ready := UInt(1)
-  io.nastimem.aw.ready := UInt(1)
-  io.nastimem.w.ready := UInt(1)
-//  io.nastimem.
+  // hide everything behind queues
+  val awQ = Module(new Queue(new NastiWriteAddressChannel, 2))
+  val wQ = Module(new Queue(new NastiWriteDataChannel, 2))
+  val bQ = Module(new Queue(new NastiWriteResponseChannel, 2))
+  val arQ = Module(new Queue(new NastiReadAddressChannel, 2))
+  val rQ = Module(new Queue(new NastiReadDataChannel, 2))
+
+  val umireqQ = Module(new Queue(new CatapultMemReq, 2))
+  val umirespQ = Module(new Queue(new CatapultMemResp, 2))
+
+  awQ.io.enq <> io.nastimem.aw
+  wQ.io.enq <> io.nastimem.w
+  io.nastimem.b <> bQ.io.deq
+  arQ.io.enq <> io.nastimem.ar
+  io.nastimem.r <> rQ.io.deq
+
+  io.umireq <> umireqQ.io.deq
+  umirespQ.io.enq <> io.umiresp
+
+  // TODO: 
+  // awQ.io.deq  // write addr IN
+  // wQ.io.deq   // write data IN
+  // bQ.io.enq   // write resp OUT
+  //
+  // arQ.io.deq // read addr IN
+  // rQ.io.enq  // read resp OUT
+  //
+  // umireqQ.io.enq // umi requests OUT
+  // umirespQ.io.deq // umi resps IN
+
+
+
+  def fire_writereq(exclude: Bool, include: Bool*) = {
+    val rvs = Seq(
+      awQ.io.deq.valid,
+      wQ.io.deq.valid,
+      bQ.io.enq.ready,
+      umireqQ.io.enq.ready
+    )
+    (rvs.filter(_ ne exclude) ++ include).reduce(_ && _)
+  }
+
+  // keep track of:
+//  awQ.io.deq.valid & wQ.io.deq.valid & bQ.io.enq.ready & umireqQ.io.enq.ready
+  awQ.io.deq.ready := fire_writereq(awQ.io.deq.valid)
+  wQ.io.deq.ready := fire_writereq(wQ.io.deq.valid)
+  bQ.io.enq.valid := fire_writereq(bQ.io.enq.ready)
+  umireqQ.io.enq.valid := fire_writereq(umireqQ.io.enq.ready)
+ 
+  // lower 6 bits must be zero since we're faking a 512 bit block
+  umireqQ.io.enq.bits.addr := awQ.io.deq.bits.addr << UInt(3)
+  umireqQ.io.enq.bits.data := wQ.io.deq.bits.data
+  umireqQ.io.enq.bits.isWrite := UInt(1) // TODO later
+
+  bQ.io.enq.bits.id := awQ.io.deq.bits.id
+  bQ.io.enq.bits.resp := UInt(0) // TODO
+  bQ.io.enq.bits.user := UInt(0) // TODO
+
+
+
+
+
+  // TODO: stick in an assert for strb
+  arQ.io.deq.ready := UInt(1)
+  rQ.io.enq.valid := UInt(0)
+
 }
 
 abstract class PlatformShim extends Module {
@@ -115,7 +177,7 @@ class ZynqShim(simIo: midas.core.SimWrapperIO)
 
   top.io.ctrl <> io.master
 
-	val nastiumi = Module(new NastiUMIAdapter)
+  val nastiumi = Module(new NastiUMIAdapter()(  p alter Map(NastiKey -> p(SlaveNastiKey))     ))
   val simumimem = Module(new SimUMIMem)
   
   nastiumi.io.nastimem <> top.io.mem
@@ -123,4 +185,8 @@ class ZynqShim(simIo: midas.core.SimWrapperIO)
   nastiumi.io.umiresp <> simumimem.io.resp
   // disconnected from top level 
   //io.slave <> top.io.mem
+  // tie off top level
+  io.slave.ar.valid := UInt(0)
+  io.slave.aw.valid := UInt(0)
+  io.slave.w.valid := UInt(0)
 }
