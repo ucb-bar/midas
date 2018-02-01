35d356754a99450fc8c6baff5464826629b94056
Donggyu Kim <dgkim@eecs.berkeley.edu>
2018-02-01 12:14:57 -0800

    pass target annotations in replay compiler

--- src/main/scala/strober/replay/Compiler.scala
+++ src/main/scala/strober/replay/Compiler.scala
@@ -3,6 +3,7 @@ package replay
 
 import firrtl._
 import firrtl.ir.Circuit
+import firrtl.annotations.Annotation
 import firrtl.passes.memlib._
 import firrtl.CompilerUtils.getLoweringTransforms
 import barstools.macros._
@@ -21,20 +22,24 @@ private class Compiler(conf: File, json: File, lib: File, macros: File, paths: F
 }
 
 object Compiler {
-  def apply(chirrtl: Circuit, io: Seq[chisel3.Data], dir: File, lib: Option[File]): Circuit = {
+  def apply(chirrtl: Circuit,
+            targetAnnos: Seq[Annotation],
+            io: Seq[chisel3.Data],
+            dir: File,
+            lib: Option[File]): Circuit = {
     dir.mkdirs
     val confFile = new File(dir, s"${chirrtl.main}.conf")
     val jsonFile = new File(dir, s"${chirrtl.main}.macros.json")
     val macroFile = new File(dir, s"${chirrtl.main}.macros.v")
     val pathFile = new File(dir, s"${chirrtl.main}.macros.path")
-    val annotations = new AnnotationMap(Seq(
+    val replayAnnos = Seq(
       InferReadWriteAnnotation(chirrtl.main),
       ReplSeqMemAnnotation(s"-c:${chirrtl.main}:-o:$confFile"),
       MacroCompilerAnnotation(chirrtl.main, MacroCompilerAnnotation.Params(
-        jsonFile.toString, lib map (_.toString), CostMetric.default, MacroCompilerAnnotation.Default))))
+        jsonFile.toString, lib map (_.toString), CostMetric.default, MacroCompilerAnnotation.Default)))
     val verilog = new FileWriter(new File(dir, s"${chirrtl.main}.v"))
     val result = new Compiler(confFile, jsonFile, lib getOrElse jsonFile, macroFile, pathFile) compile (
-      CircuitState(chirrtl, ChirrtlForm, Some(annotations)), verilog)
+      CircuitState(chirrtl, ChirrtlForm, Some(new AnnotationMap(targetAnnos ++ replayAnnos))), verilog)
     genVerilogFragment(chirrtl.main, io, new FileWriter(new File(dir, s"${chirrtl.main}.vfrag")))
     verilog.close
     result.circuit
@@ -43,8 +48,9 @@ object Compiler {
   def apply[T <: chisel3.core.UserModule](
       w: => T, dir: File, lib: Option[File] = None): Circuit = {
     lazy val dut = w
-    val chirrtl = Parser.parse(chisel3.Driver.emit(() => dut))
+    val circuit = chisel3.Driver.elaborate(() => dut)
+    val chirrtl = Parser.parse(chisel3.Driver.emit(circuit))
     val io = dut.getPorts map (_.id)
-    apply(chirrtl, io, dir, lib)
+    apply(chirrtl, circuit.annotations.toSeq, io, dir, lib)
   }
 }
