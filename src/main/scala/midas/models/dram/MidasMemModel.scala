71b8fc846f9cd5c95775232235a9501b793a205e
Howard Mao <zhehao.mao@gmail.com>
2019-03-11 19:37:52 +0000

    don't generate redundant TARGET_MEM_ADDR_BITS

--- src/main/scala/midas/models/dram/MidasMemModel.scala
+++ src/main/scala/midas/models/dram/MidasMemModel.scala
@@ -429,9 +429,6 @@ class MidasMemModel(cfg: BaseConfig)(implicit p: Parameters) extends MemModel {
     sb.append(CppGenerationUtils.genMacro(s"${getWName.toUpperCase}_target_addr_bits", UInt32(p(NastiKey).addrBits)))
 
     crRegistry.genArrayHeader(wName.getOrElse(name).toUpperCase, base, sb)
-
-    val targetAddrBits = model.io.tNasti.nastiXAddrBits
-    sb.append(genMacro("TARGET_MEM_ADDR_BITS", UInt32(targetAddrBits)))
   }
 
   // Prints out key elaboration time settings
